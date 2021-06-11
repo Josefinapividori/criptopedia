89b00044738fc23b3363c2361ff805ddc0654330
josefina pividori <josefinaaapividori@gmail.com>
2021-06-11 13:04:25 -0300

    ordeno

--- js/main.js
+++ js/main.js
@@ -3,4 +3,6 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
 
     document.body.classList.toggle('darck');
+
+    btnSwitch.classList.toggle('active');
 });
