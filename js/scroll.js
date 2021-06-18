38ac5fc8020b9f75786e5fc6f3ed02c349800b36
josefina pividori <josefinaaapividori@gmail.com>
2021-06-18 13:42:33 -0300

    sumo boton estatico scrollup

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+const TOUP_Y = 10
+const MAX_SCROLL = 150
+function fn_goup(){
+    window.scrollTo(0, TOUP_Y)
+}
+
+(function() {
+    //a partir de que punto del scroll vertical de la ventana mostrará el botón
+    const ishow = 115
+    const $divtop = document.getElementById("div-totop")
+    window.addEventListener("scroll", function() {
+        if(document.documentElement.scrollTop > ishow){
+            $divtop.style.display = "inherit"
+        }
+        else {
+            $divtop.style.display = "none"
+        }
+    })
+})();
